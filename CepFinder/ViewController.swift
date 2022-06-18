0c13f29004bcd8924e11eade053bde32bba0bda1
Milene Rodrigues <miiers3112@gmail.com>
2022-06-18 15:07:47 -0300

    Test field did end editing

--- CepFinder/ViewController.swift
+++ CepFinder/ViewController.swift
@@ -9,37 +9,44 @@ import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate, CepServiceProtocol {
 
-    @IBOutlet weak var cepLabel: UILabel!
     @IBOutlet weak var cepField: UITextField!
-    
-    @IBOutlet weak var ruaLabel: UILabel!
     @IBOutlet weak var ruaField: UITextField!
-    
-    @IBOutlet var bairroLabel: UILabel!
     @IBOutlet weak var bairroField: UITextField!
-    
-    @IBOutlet weak var cidadeLabel: UILabel!
     @IBOutlet weak var cidadeField: UITextField!
-    
-    @IBOutlet weak var estadoLabel: UILabel!
     @IBOutlet weak var estadoField: UITextField!
     
     let service = CepService()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.service.delegate = self
+        service.delegate = self
         cepField.delegate = self
-       
     }
-
+    
+    // Cep service
     func cepSuccess(endereco: Endereco) {
-        
+        DispatchQueue.main.async {
+            self.ruaField.text = endereco.logradouro
+            self.bairroField.text = endereco.bairro
+            self.cidadeField.text = endereco.localidade
+            self.estadoField.text = endereco.uf
+        }
     }
     
     func cepError(messege: String) {
-        
+        DispatchQueue.main.async {
+            print("Cep não encontrado")
+        }
     }
 
+    // UITextField
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+        service.callCep(cep: textField.text ?? "")
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
--- Model.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Model.swift
-//  CepFinder
-//
-//  Created by Virtual Machine on 15/06/22.
-//
-
-import Foundation
